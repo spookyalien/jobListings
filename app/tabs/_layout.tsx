94e5e72e7b0cabae1e15acf7909eb208b57abcbb
spookyalien <gameplayftw101@gmail.com>
2024-12-12 11:22:24 -0500

    Navigation tabs

--- app/tabs/_layout.tsx
+++ app/tabs/_layout.tsx
@@ -2,24 +2,26 @@ import FontAwesome from '@expo/vector-icons/FontAwesome';
 import { Tabs } from 'expo-router';
 
 export default function TabLayout() {
-  return (
-    <Tabs screenOptions={{ tabBarActiveTintColor: 'blue' }} >
-      <Tabs.Screen
-        name="index"
-        options={{
-          title: 'Job Postings',
-          tabBarIcon: ({ color }) => <FontAwesome size={28} name="briefcase" color={color} />,
-          headerShown: false,
-        }}
-      />
-      <Tabs.Screen
-        name="profile"
-        options={{
-          title: 'Profile',
-          tabBarIcon: ({ color }) => <FontAwesome size={28} name="user" color={color} />,
-          headerShown: false,
-        }}
-      />
-    </Tabs>
-  );
+    const iconSize = 28;
+    
+    return (
+        <Tabs screenOptions={{ tabBarActiveTintColor: 'blue' }} >
+        <Tabs.Screen
+            name="index"
+            options={{
+                title: 'Job Postings',
+                tabBarIcon: ({ color }) => <FontAwesome size={iconSize} name="briefcase" color={color} />,
+                headerShown: false,
+            }}
+        />
+        <Tabs.Screen
+            name="profile"
+            options={{
+                title: 'Profile',
+                tabBarIcon: ({ color }) => <FontAwesome size={iconSize} name="user" color={color} />,
+                headerShown: false,
+            }}
+        />
+        </Tabs>
+    );
 }
